b001cb504c511ef57c8107836ea3113c7203a8cd
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-09-21 17:13:52 +0530

    sql upd

---
+++ company.sql
@@ -0,0 +1,11 @@
+USE Company;
+
+CREATE TABLE Customers (Cid varchar(5) primary key, Cname varchar(28) NOT NULL,
+city varchar(20) NOT NULL, state varchar(15), pincode int(8), product varchar(20),
+class char(1) DEFAULT 'A');
+
+SHOW TABLE;
+
+
+
+
