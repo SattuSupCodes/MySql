92a591731af2e756cf6403635a7ea48fc78b6f2e
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-09-23 20:24:56 +0530

    sql upd

--- company.sql
+++ company.sql
@@ -5,7 +5,8 @@ city varchar(20) NOT NULL, state varchar(15), pincode int(8), product varchar(20
 class char(1) DEFAULT 'A')
 
 
--- idk why show tables shows syntax error
+-- idk why show tables shows syntax errorhhhh
 
 
 
+insert into customers values(567, 'Shatakshi', 'Noida', 'UP', 201016, 'Bag', '1');
